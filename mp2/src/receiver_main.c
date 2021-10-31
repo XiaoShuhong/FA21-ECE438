629e5645b6d704d2a6b0d5c5b61e97af9c7206bf
XiaoShuhong <shuhong3@illinois.edu>
2021-10-31 17:04:23 +0800

    test1

--- mp2/src/receiver_main.c
+++ mp2/src/receiver_main.c
@@ -1,8 +1,8 @@
 /* 
  * File:   receiver_main.c
- * Author: 
+ * Author: zicheng5
  *
- * Created on
+ * Created on 2021.10.30
  */
 
 #include <stdio.h>
@@ -15,22 +15,66 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/time.h>
+#include <iostream>
+#include <sys/time.h>
+#include <queue>
+#include <map>
+#include <algorithm>
+using namespace std;
 
-
+#define BUFFERSIZE 999 // max number of packets that can be buffered at the receiver side
+#define MAX_SIZE 500
+#define DATA 0
+#define ACK 1
+#define SYN 2
+#define SYNACK 3
+#define FIN 4
+#define FINACK 5
+#define MAX_SEQ 1000
 
 struct sockaddr_in si_me, si_other;
 int s, slen;
+FILE * fPtr;
 
 void diep(char *s) {
     perror(s);
     exit(1);
 }
 
+typedef struct{
+    int ptype;   //data 0，ack 1，syn 2, synack 3,fin 4, finack 5
+    int psize;
+    union ID{
+        int data_id;
+        int ack_id;
+    }id;
+    char data[MAX_SIZE];
+    // struct timeval time；//time.tv_sec and time.utv_sec
+}packet;
 
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
     
     slen = sizeof (si_other);
+    int numbytes;
+    int ACKseq = 0;
+    int buf_packet_num = 0;
+    // vector<packet> buf(BUFFERSIZE);     //buffering received packets
+    // char buf[BUFFERSIZE];
+    map<int,packet> buf;
+    map<int,int> flag;
+    // fill(buf,buf + BUFFERSIZE,0);
+    char recvtemp[sizeof(packet)];      //recv() function buffer
+    char sendtemp[sizeof(packet)];      //sendto() function buffer
+
+    /* Initialize the flag map */
+    for(int i = 0; i<MAX_SEQ; i++){
+        flag.insert(pair<int,int>(i,0));
+    }
 
 
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
@@ -45,8 +89,177 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         diep("bind");
 
 
-	/* Now receive data and send acknowledgements */    
+	/* Now receive data and send acknowledgements */ 
 
+    // if ((numbytes = recvfrom(s, recvtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t*)&slen)) == -1) {
+	//     diep("recv");
+	// }
+
+    // packet recv_syn_packet;
+    // memcpy((void*)&recv_syn_packet,recvtemp,sizeof(packet));
+
+    // /* receiving a SYN */
+    // if (recv_syn_packet.ptype == SYN) {  
+    //     packet synack_packet;
+    //     synack_packet.ptype = SYNACK;
+    //     synack_packet.psize = 0;
+    //     memcpy(sendtemp,&synack_packet,sizeof(packet)); 
+
+    //     if (sendto(s, sendtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)slen)==-1){
+    //         diep("SYNACK's sendto()");
+    //     }  
+    // }
+    cout<<"Now entering the loop "<< endl;
+    while(1){
+        cout<<"Now successfully entering the loop "<< endl;
+        if ((numbytes = recvfrom(s, recvtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t*)&slen)) == -1) {
+            diep("recv");
+        }
+        packet recv_packet;
+        memcpy((void*)&recv_packet,recvtemp,sizeof(packet));
+
+        /* receiving a SYN */
+        if (recv_packet.ptype == SYN) {  
+            cout<<"receive SYN "<< endl;
+            packet synack_packet;
+            synack_packet.ptype = SYNACK;
+            synack_packet.psize = 0;
+            memcpy(sendtemp,&synack_packet,sizeof(packet)); 
+
+            if (sendto(s, sendtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)slen)==-1){
+                diep("SYNACK's sendto()");
+            }  
+        }
+
+        /* receiving a FIN */
+        if (recv_packet.ptype == FIN) { 
+            cout<<"receive FIN "<< endl; 
+            packet finack_packet;
+            finack_packet.ptype = FINACK;
+            finack_packet.psize = 0;
+            memcpy(sendtemp,&finack_packet,sizeof(packet));
+
+            if (sendto(s, sendtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)slen)==-1){
+                diep("FINACK's sendto()");
+            } 
+            break; 
+        }
+
+        /* receiving a normal data packet */
+        if (recv_packet.ptype == DATA) {
+            /* 1.receiving an out of date packets */
+            if (recv_packet.id.data_id < ACKseq){
+                cout<<"received data_id "<< recv_packet.id.data_id <<endl;
+                cout<<"Now the ACKseq is "<< ACKseq <<endl;
+                cout<<"receive out of date packet "<< recv_packet.id.data_id <<endl;
+                /* send normal ACK back to sender */
+                packet pastack_packet;
+                pastack_packet.ptype = ACK;
+                pastack_packet.psize = 0;
+                pastack_packet.id.ack_id = ACKseq - 1;
+                memcpy(sendtemp,&pastack_packet,sizeof(packet));
+                cout<<"prepare to send ACK "<< ACKseq-1 <<endl;
+
+                if (sendto(s, sendtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)slen)==-1){
+                    diep("past ACK's sendto()");
+                }
+
+                cout<<"send ACK "<< ACKseq-1 <<" successfully!"<<endl;
+            }
+            /* 2.receiving exactly the right packet */
+            else if (recv_packet.id.data_id == ACKseq ) {
+                cout<<"received data_id "<< recv_packet.id.data_id <<endl;
+                cout<<"Now the ACKseq is "<< ACKseq <<endl;
+                cout<<"receive right packet "<< recv_packet.id.data_id <<endl;
+
+                int cnt = ACKseq;
+                buf[ACKseq % MAX_SEQ] = recv_packet;
+                flag[ACKseq % MAX_SEQ] = 1;
+                // memcpy(buf+ACKseq,&recv_packet,sizeof(packet));
+                while(flag[cnt % MAX_SEQ]!=0){
+                    cnt++;
+                }
+                /* send normal ACK back to sender */
+                packet goodack_packet;
+                goodack_packet.ptype = ACK;
+                goodack_packet.psize = 0;
+                goodack_packet.id.ack_id = cnt-1;
+                memcpy(sendtemp,&goodack_packet,sizeof(packet));
+                cout<<"prepare to send ACK "<< cnt-1 <<endl;
+
+                if (sendto(s, sendtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)slen)==-1){
+                    diep("good ACK's sendto()");
+                }
+
+                cout<<"send ACK "<< cnt-1 <<" successfully!"<<endl;
+
+                /* Write the packet data into destinationFile, if there's a packet in the next position,
+                write it too. Do it till no packet buffered in the next position, 
+                then ACKseq++ and buf_packet_num-- */
+                while(flag[ACKseq % MAX_SEQ] != 0){
+                    fPtr =  fopen(destinationFile, "a");
+                    if(fPtr == NULL){
+                        diep("Could not open file to write.");
+                    }
+                    fputs(((packet*) &buf[ACKseq % MAX_SEQ])->data, fPtr);
+                    
+                    cout<<"File written successfully for ACKseq "<< ACKseq <<endl;
+                    cout<<endl;
+
+                    fclose(fPtr);
+                    flag[ACKseq % MAX_SEQ] = 0;
+                    // buf[ACKseq] = 0;
+                    ACKseq++;
+                    buf_packet_num--;
+                }
+                // *(buf + 0) = recv_packet;
+                // while(buf[0] != 0){
+                //     fPtr =  fopen(destinationFile, "w");
+                //     if(fPtr == NULL){
+                //         diep("Could not open file to write.");
+                //     }
+                //     fputs(buf[0], fPtr);
+                //     fclose(fPtr);
+
+                //     buf[0] = 0;
+                //     ACKseq = ((ACKseq+1) % MAX_SEQ);
+                //     buf_packet_num--;
+                //     rotate(buf.begin(),buf.begin()+1,buf.end())
+                //  }
+            }
+            /* 3.receiving future packets */
+            else if (recv_packet.id.data_id > ACKseq) {
+                cout<<"received data_id "<< recv_packet.id.data_id <<endl;
+                cout<<"Now the ACKseq is "<< ACKseq <<endl;
+                cout<<"receive future packet "<< recv_packet.id.data_id <<endl;
+                buf_packet_num++;
+                if (buf_packet_num == BUFFERSIZE){
+                    diep("Full receiver buffer");
+                }
+
+                /* send dup ACK back to sender */
+                packet dupack_packet;
+                dupack_packet.ptype = ACK;
+                dupack_packet.psize = 0;
+                dupack_packet.id.ack_id = ACKseq-1;
+                memcpy(sendtemp,&dupack_packet,sizeof(packet));
+
+                if (sendto(s, sendtemp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)slen)==-1){
+                    diep("dup ACK's sendto()");
+                }
+
+                /* buffer the received packets */
+                if (flag[recv_packet.id.data_id % MAX_SEQ] == 0){
+                    // memcpy(buf + (recv_packet.id.data_id % MAX_SEQ),&recv_packet,sizeof(packet));
+                    buf[recv_packet.id.data_id % MAX_SEQ] = recv_packet;
+                    flag[recv_packet.id.data_id % MAX_SEQ] = 1;
+                }
+                // if (buf[recv_packet.id.data_id - (ACKseq % MAX_SEQ)] == 0){
+                //     *(buf + recv_packet.id.data_id - (ACKseq % MAX_SEQ)) = recv_packet;
+                // }
+            }
+        }
+    }      
     close(s);
 	printf("%s received.", destinationFile);
     return;
--- mp2/src/sender_main.c
+++ mp2/src/sender_main.c
@@ -19,19 +19,78 @@
 #include <signal.h>
 #include <string.h>
 #include <sys/time.h>
+#include<iostream>
+#include <sys/time.h>
+#include <queue>
+#include<math.h>
+
+using namespace std;
 
 struct sockaddr_in si_other;
 int s, slen;
 
+//////////////////
+#define MAX_SIZE 500
+#define DATA 0
+#define ACK 1
+#define SYN 2
+#define SYNACK 3
+#define FIN 4
+#define FINACK 5
+#define MAX_SEQ 1000
+#define RTT 20000
+#define RTO_TH 2*RTT
+#define DATA_QUEUE_SIZE 200
+
+FILE *fp;
+unsigned long long int bytesToPacket;
+unsigned long long int seq_num=0;
+int queue_done_flag=0;
+int flow=0;
+
+
+typedef struct{
+    int ptype;   //data 0，ack 1，syn 2, synack 3,fin 4, finack 5
+    int psize;
+    union ID{
+        int data_id;
+        int ack_id;
+    }id;
+    char data[MAX_SIZE];
+    struct timeval time;//time.tv_sec and time.utv_sec
+}packet;
+
+queue <packet> data_queue;
+queue <packet> ack_queue;
+
+double CW=1.0;
+double SST=64.0;
+int dupACKCount=0;
+
+enum STATE{Slow_Start,Congestion_Avoidance,Fast_Recovery}current_state=Slow_Start;
+int timeout = 0;
+int new_ack = 0;  //flags
+char data_temp[sizeof(packet)]; 
+//////////////////
 void diep(char *s) {
     perror(s);
     exit(1);
 }
 
+void start_connection(int s);
+void finish_connection(int s);
+void settimeout(int s);
+void queuing_packet(int num);
+void sendpacket(int socket);
+void state_transition(int s);
+
+
+
+
 
 void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
     //Open the file
-    FILE *fp;
+
     fp = fopen(filename, "rb");
     if (fp == NULL) {
         printf("Could not open file to send.");
@@ -39,25 +98,83 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
     }
 
 	/* Determine how many bytes to transfer */
+    bytesToPacket=bytesToTransfer;
 
-    slen = sizeof (si_other);
+    slen = sizeof (si_other); //s_other as the socket at sender
 
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    //open a socket using udp, bind the third parameter with 0 works either
         diep("socket");
 
     memset((char *) &si_other, 0, sizeof (si_other));
     si_other.sin_family = AF_INET;
     si_other.sin_port = htons(hostUDPport);
+
     if (inet_aton(hostname, &si_other.sin_addr) == 0) {
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
     }
 
+    start_connection(s); //handshake
+
+    queuing_packet(1);
+
+    settimeout(s); //lyc他们的version
+
+    sendpacket(s);
+    while(!data_queue.empty() || !ack_queue.empty()){
+        memset(&data_temp, 0, sizeof(packet));
+        if(recvfrom(s, data_temp, sizeof(packet), 0, NULL , NULL)==-1){
+            if(errno != EAGAIN || errno != EWOULDBLOCK){
+                diep("can not receive ack");
+            }
+            timeout=1;
+            new_ack=0;
+            cout<<"timeout"<<endl;
+            state_transition(s);
+        }
+        else{
+            packet cur_packet;
+            memcpy(&cur_packet, data_temp, sizeof(packet));
+            if(cur_packet.ptype==ACK){
+                cout<<"receive ack"<<cur_packet.id.ack_id<<endl;
+                if(cur_packet.id.ack_id==ack_queue.front().id.data_id){
+                    new_ack=1;
+                    ack_queue.pop();
+                    state_transition(s);
+                }
+                else if(cur_packet.id.ack_id>ack_queue.front().id.data_id){
+                    while(!ack_queue.empty() && ack_queue.front().id.data_id<=cur_packet.id.ack_id){
+                        ack_queue.pop();
+                        new_ack=1;
+                        state_transition(s);
+                    }
+
+                    
+                    
+                }
+                else{
+                    new_ack=0;
+                    timeout=0;
+                    state_transition(s);
+                }
+
+            }
+            
+        }
+
+
+
+    }
+
 
 	/* Send data and receive acknowledgements on s*/
+    finish_connection(s);
 
     printf("Closing the socket\n");
     close(s);
+    fclose(fp);
+    cout<<"done transmit"<<endl;
     return;
 
 }
@@ -80,9 +197,259 @@ int main(int argc, char** argv) {
 
 
     reliablyTransfer(argv[1], udpPort, argv[3], numBytes);
-
+    
 
     return (EXIT_SUCCESS);
 }
 
+void start_connection(int s){
+    packet start_syn;
+    char temp[sizeof(packet)];
+    start_syn.ptype=SYN;
+    start_syn.psize=0;
+    memcpy(temp, &start_syn, sizeof(packet));
+    if(sendto(s,temp,sizeof(packet),0,(sockaddr*)&si_other, (socklen_t)slen)==-1){
+        diep("start connection error when send syn");
+    }
+    if(recvfrom(s, temp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t*)&slen)==-1){
+        diep("error when receive synack");
+    }
+    packet start_ack;
+    start_ack.ptype=ACK;
+    start_ack.psize=0;
+    memcpy(temp, &start_ack, sizeof(packet));
+    if(sendto(s,temp,sizeof(packet),0,(sockaddr*)&si_other, (socklen_t)slen)==-1){
+        diep("start connection error when send ack");
+    }
+    cout<<"successfully connected"<<endl;
 
+}
+
+void finish_connection(int s){
+    packet done_fin;
+    char temp[sizeof(packet)];
+    done_fin.ptype=FIN;
+    done_fin.psize=0;
+    memcpy(temp, &done_fin, sizeof(packet));
+    if(sendto(s,temp,sizeof(packet),0,(sockaddr*)&si_other, (socklen_t)slen)==-1){
+        diep("start connection error when send syn");
+    }
+    // if(recvfrom(s, temp, sizeof(packet), 0,(sockaddr*)&si_other, (socklen_t*)&slen)==-1){
+    //     diep("error when receive synack");
+    // }
+    while (recvfrom(s, temp, sizeof(packet), 0, NULL , NULL)==-1){
+        if(errno != EAGAIN || errno != EWOULDBLOCK){
+            diep("error when receive finack");
+        }
+        if(sendto(s,temp,sizeof(packet),0,(sockaddr*)&si_other, (socklen_t)slen)==-1){
+            diep("start connection error when send syn");
+        }
+        
+    }
+    packet done_ack;
+    done_ack.ptype=ACK;
+    done_ack.psize=0;
+    memcpy(temp, &done_ack, sizeof(packet));
+    if(sendto(s,temp,sizeof(packet),0,(sockaddr*)&si_other, (socklen_t)slen)==-1){
+        diep("start connection error when send ack");
+    }
+    cout<<"successfully connected"<<endl;
+}
+void settimeout(int s){
+    struct timeval RTO;
+    RTO.tv_sec=0;
+    RTO.tv_usec=RTO_TH;
+    if(setsockopt(s,SOL_SOCKET,SO_RCVTIMEO,&RTO,sizeof(RTO))==-1){
+        diep("settimeout()");
+    }
+
+}
+
+void queuing_packet(int num){     //修改：目前是一次性把整个文件拆好入栈，但如果需要考虑max_seq动态变化，则每次入栈最多max_queue个包
+    cout<<"now in queue function"<<endl;
+    cout<<queue_done_flag<<num<<bytesToPacket<<endl;
+    if (queue_done_flag==1){
+        return;
+    }
+    if (num<1 ){
+        cout<<"due to CW, no new queuing"<<endl;
+        return;
+    } 
+    if (bytesToPacket==0){
+        cout<<"queeing all done"<<endl;
+        queue_done_flag=1;
+        return ;
+    }                                              //或者，在实际传输以前修改id以匹配
+
+    while(bytesToPacket>0 && num>0){
+        packet my_packet;
+        char buf[MAX_SIZE];
+        int idx;
+        int byte_read;
+        int read_size;
+        if(bytesToPacket<MAX_SIZE){
+            byte_read=bytesToPacket;
+        }
+        else{
+            byte_read=MAX_SIZE;
+        }
+        // cout<<"queuing "<<num<<" packets begin, still"<<bytesToPacket<<"Byte not queuing"<<endl;
+        read_size=fread(buf, sizeof(char), byte_read, fp);
+        if(read_size>0){
+            my_packet.ptype = DATA;
+            my_packet.psize = read_size;
+            my_packet.id.data_id = seq_num;
+            seq_num = seq_num+1;
+            memcpy(my_packet.data,&buf,sizeof(char)*byte_read);
+            data_queue.push(my_packet);
+            cout<<"flow "<<flow<<" queueing packet "<<my_packet.id.data_id<<endl;
+            
+            bytesToPacket=bytesToPacket-read_size;
+            num-=1;
+        }
+    }
+    cout<<"flow "<<flow<<" queueing finish"<<endl;
+    flow+=1;
+    
+}
+
+void sendpacket(int socket){
+    int num_p_send=CW-ack_queue.size();
+    queuing_packet(num_p_send);
+    cout<<"flow "<<flow<<"CW is "<<CW<<","<<ack_queue.size()<<" not receive ack, "<<"send "<<num_p_send<<" packets"<<endl;
+    
+    char temp[sizeof(packet)]; 
+    if (num_p_send<1.0){
+        if (timeout==1){
+            cout<<"resend packet "<<ack_queue.front().id.data_id<<" due to time out"<<endl;
+        }
+        if (dupACKCount==3){
+            cout<<"resend packet "<<ack_queue.front().id.data_id<<" due to duplicate"<<endl;
+        }
+        memcpy(temp, &ack_queue.front(), sizeof(packet));
+        if (sendto(socket, temp, sizeof(packet), 0,(sockaddr*)&si_other, (socklen_t)slen)==-1){
+            diep("sendto()");
+        }
+        cout<<"resend packet "<<ack_queue.front().id.data_id<<" done"<<endl;
+                
+        //如果处于timeout，则再发送ack_queue最前面的packet
+    }
+    else{
+        if(data_queue.empty()){
+            cout<<"data packets all send"<<endl;
+        }
+        else{
+            if (num_p_send>data_queue.size()){
+                num_p_send=data_queue.size();
+            }
+            for(int i=0;i<num_p_send;++i){
+                memcpy(temp,&data_queue.front(),sizeof(packet));
+                if (sendto(socket, temp, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)slen)==-1){
+                    diep("sendto()");
+                }    
+                cout<<"flow "<<flow<<", send packet "<<data_queue.front().id.data_id<<"done"<<endl;
+                ack_queue.push(data_queue.front());
+                data_queue.pop();           
+            }
+        }
+    }
+    flow+=1;
+    
+
+}
+
+
+
+
+
+void state_transition(int s){
+    cout<<"flow "<<flow<<" now we are in state "<<current_state<<endl;
+    switch(current_state){
+        case Slow_Start:
+            if(new_ack){
+                CW++;
+                dupACKCount=0;
+                sendpacket(s);
+                new_ack=0;
+            }
+            else{
+                dupACKCount++;
+            }
+            
+            if(timeout){
+                SST=CW/2.0;
+                CW=1;
+                dupACKCount=0;
+                sendpacket(s);
+                timeout=0;
+            }
+            if(CW>=SST){
+                current_state=Congestion_Avoidance;
+                return;
+            }
+            if(dupACKCount==3){
+                SST=CW/2.0;
+                CW=SST+3.0;
+                sendpacket(s);
+                current_state=Fast_Recovery;
+                return;
+            }
+            break;
+        case Congestion_Avoidance:
+            if(new_ack){
+                CW=CW+1.0/floor(CW);
+                dupACKCount=0;
+                sendpacket(s);
+                new_ack=0;
+            }
+            else{
+                dupACKCount++;
+            }
+            if(timeout){
+                SST=CW/2.0;
+                CW=1.0;
+                dupACKCount=0;
+                sendpacket(s);
+                current_state=Slow_Start;
+                timeout=0;
+                return;
+            }
+            
+            if(dupACKCount==3){
+                SST=CW/2.0;
+                CW=SST+3.0;
+                sendpacket(s);
+                current_state=Fast_Recovery;
+                return;
+            }
+            break;
+        case Fast_Recovery:
+            
+            if(new_ack){
+                CW=SST;
+                dupACKCount=0;
+                sendpacket(s);
+                current_state=Congestion_Avoidance;
+                new_ack=0;
+                return;
+            }
+            else{
+                dupACKCount++;
+                CW++;
+                sendpacket(s);
+            }
+            if(timeout){
+                SST=CW/2.0;
+                CW=1.0;
+                dupACKCount=0;
+                sendpacket(s);
+                current_state=Slow_Start;
+                timeout=0;
+                return;
+            }
+            break;
+        default:
+            break;
+
+    }
+}

3c8f42231736368f9fad5a4712727874f47323d2
XiaoShuhong <shuhong3@illinois.edu>
2021-11-02 14:15:15 +0800

    test4

---
+++ mp1/src/http_server.c
@@ -0,0 +1,191 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <iostream>
+using namespace std;
+
+#define PORT "3490"  // the port users will be connecting to
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+#define MAXBUFSIZE 1000
+
+void sigchld_handler(int s)
+{
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+	    fprintf(stderr,"incorrect\n");
+	    exit(1);
+	}
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+///@@@@@//
+    string pport=argv[1];
+	if ((rv = getaddrinfo(NULL, pport.c_str(), &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+	char buf1[MAXBUFSIZE],buf2[MAXBUFSIZE];
+	string bad="HTTP/1.1 400 Bad Request\r\n\r\n";
+	string not_f="HTTP/1.1 404 Whoops, file not found\r\n\r\n";
+	string good="HTTP/1.1 200 OK\r\n\r\n";
+
+	while(1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+		FILE *f;
+		if (!fork()) { // this is the child process
+			close(sockfd); // child doesn't need the listener
+			// if (send(new_fd, "Hello, world!", 13, 0) == -1)
+			// 	perror("send");
+			// close(new_fd);
+			// exit(0);
+			memset(buf1,'\0',MAXBUFSIZE);  //
+			recv(new_fd,buf1,MAXBUFSIZE,0);
+			
+			string my_req(buf1);
+			//
+			cout<<my_req<<endl;
+			cout<<"finish head"<<endl;
+			int pos1,pos2;
+			pos1=my_req.find("GET ");
+			pos2=my_req.find(" HTTP/1.1");
+			if(pos1==my_req.npos || pos2==my_req.npos || pos1>pos2){
+				if(send(new_fd,bad.c_str(),bad.size(), 0) == -1){
+					perror("send");
+				}
+				exit(0);
+
+			}
+			string my_path=my_req.substr(pos1+5,pos2-pos1-5);
+			cout<<my_path<<endl;
+			
+			f=fopen(my_path.c_str(),"rb");
+			if(f==NULL){
+				if(send(new_fd, not_f.c_str(),not_f.size(), 0) == -1){
+					perror("send");
+				}
+				perror("fopen");
+				exit(0);
+			}
+			else{
+			
+				if(send(new_fd, good.c_str(),good.size(), 0) == -1){
+					perror("send");
+					exit(0);
+				}
+				
+				int num_read;
+				memset(buf2,'\0',MAXBUFSIZE);  //
+				while ((num_read=fread(buf2,sizeof(char),MAXBUFSIZE,f))!=0){
+					if (send(new_fd,buf2,num_read,0)==-1){
+						perror("send");
+						exit(0);
+					}
+					memset(buf2,'\0',MAXBUFSIZE);
+				}
+				fclose(f);
+				close(new_fd);
+				exit(0);
+			}
+			
+
+
+		}
+		close(new_fd);  // parent doesn't need this
+	}
+
+	return 0;
+}
+
